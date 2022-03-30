72f4ee4f20a396238af96fcb13912b3e6d5c1f49
bgeorgieff <b.g.georgiev87@gmail.com>
2022-03-30 10:36:45 +0300

    angular services task solution

---
+++ src/app/counter.service.ts
@@ -0,0 +1,14 @@
+export class CounterService {
+  activeToInactive = 0;
+  inactiveToActive = 0;
+
+  incrementActiveToInactive() {
+    this.activeToInactive++;
+    console.log("Active to inactive " + this.activeToInactive);
+  }
+
+  incrementInactiveToActive() {
+    this.inactiveToActive++;
+    console.log("Inactive to active " + this.inactiveToActive);
+  }
+}
